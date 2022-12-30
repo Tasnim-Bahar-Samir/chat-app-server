04c2925aed100ca3b42337cb6da559ca70a0bf66
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-12-31 00:05:26 +0600

    sending data to the fronend

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const express = require("express")
 const http = require('http')
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const {Server} = require('socket.io')
+const socketIO = require('socket.io')
 
 const app = express()
 
-const server = http.createServer(app)
-const io = new Server(server);
-
 app.use(cors())
 app.use(express.json())
+
+const server = http.createServer(app)
+const io = socketIO(server);
+
+io.on("connection",(socket)=>{
+    console.log('New connection added')
+    // socket.on("joined",({user})=>{
+    //     console.log(`${user} has joined`)
+    // })
+})
+
+
 
 app.get('/', (req,res)=>{
     res.send("Chat server is running");
