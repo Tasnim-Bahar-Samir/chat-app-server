9771abee5b58a2626bc2da2b30487152fadfb09e
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2023-01-02 20:51:24 +0600

    update some changes

--- index.js
+++ index.js
@@ -12,11 +12,22 @@ app.use(express.json())
 const server = http.createServer(app)
 const io = socketIO(server);
 
+const users = [{}]
+
 io.on("connection",(socket)=>{
     console.log('New connection added')
-    // socket.on("joined",({user})=>{
-    //     console.log(`${user} has joined`)
-    // })
+    socket.on("joined",({user})=>{
+        console.log(`${user} has joined`)
+        users[socket.id] = user;
+        socket.emit('welcomeMsg',{user:'Admin', message:"Welcome to the chat", })
+    socket.broadcast.emit('joinMsg', {user:'user', message:"User has joined"})
+    })
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('disconnected',{user:'Admin',message:"User has left"})
+    })
+    socket.on('message',({message,id})=>{
+        io.emit('send',{user:users[id],message,id})
+    })
 })
 
 
